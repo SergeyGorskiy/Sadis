8b726e5b83f224669daafe690658f56433234cc9
Sergey Nazhestkin <9497020@mail.ru>
2023-12-01 23:49:29 +0300

    Add project files.

---
+++ Sadis.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Sadis.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
